22d61195751cb121453707e54a98f68148930a0e
Santiago Mesa Parra <santiagomesa0911@gmail.com>
2022-06-15 16:36:35 -0500

    Ejercicios tema 4

---
+++ Introducción_a_la_programación/Ejerciciostema4/EjercicioFor/src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        for (int numeroFor = 0; numeroFor <= 3; numeroFor++) {
+            System.out.println(numeroFor);
+        }
+    }
+}
