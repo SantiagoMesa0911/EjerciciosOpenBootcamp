0a6bb2ce269ba9825d921807beb658ec08b16b63
Santiago Mesa Parra <santiagomesa0911@gmail.com>
2022-06-15 13:17:46 -0500

    Ejercicios tema 4

---
+++ Introducción_a_la_programación/Ejerciciostema4/EjercicioDoWhile/src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int numeroWhile = 1;
+        do {
+            numeroWhile++;
+            numeroWhile++;
+            System.out.println(numeroWhile);
+        } while (numeroWhile< 3);
+    }
+}
---
+++ Introducción_a_la_programación/Ejerciciostema4/EjercicioWhile/src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int numeroWhile = 1;
+        while (numeroWhile < 3) {
+            numeroWhile++;
+            System.out.println(numeroWhile);
+        }
+    }
+}
